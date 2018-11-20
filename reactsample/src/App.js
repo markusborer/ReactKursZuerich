3cabf1ee84262e9a758a799dac92f9e00a32f6bb
Markus Borer <markus.borer@adesso.ch>
2018-11-20 09:27:44 +0100

    Uebung 1

--- reactsample/src/App.js
+++ reactsample/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer';
 
 class App extends Component {
     render() {
@@ -10,7 +11,7 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                 </header>
                 <main>
-
+                    <Timer />
                 </main>
             </div>
         );
---
+++ reactsample/src/Timer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Timer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>{new Date().toLocaleTimeString()}</div>
+    }
+
+}
+
+export default Timer;
